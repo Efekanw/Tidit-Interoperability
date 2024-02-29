960c6af1f133094a8f2db425ec7e38a635e5d5f6
Efekan Sargın <56073720+Efekanw@users.noreply.github.com>
2024-02-29 10:13:19 +0300

    .

--- dit.py
+++ dit.py
@@ -3,41 +3,28 @@ from requests.auth import HTTPBasicAuth
 import requests
 import json
 
-ditto_url = 'http://localhost:8080/api/2/things/sensor:4/'
+ditto_url = 'http://localhost:8080/api/2/things/sensor:4/features/Voltage/definition'
 username = "admin"
 password = "VLmJN7HfFDAOrNI"
 
 
 def listen_to_event_stream(url):
+
     auth = HTTPBasicAuth("ditto", "ditto")
+    response = requests.get(url, auth=auth)
 
-    messages = SSEClient(url, auth=auth)
+    # messages = SSEClient(url, auth=auth)
+    content = response.text.strip('[]')
+    content = content.strip('"')
+    print(content)
 
-    try:
-        for msg in messages:
-            data = msg.data.strip()
-            if data:
-                #params = {
-                #       "@context": {
-                #         "qudt": "http://qudt.org/schema/qudt#",
-                #         "unit": "http://qudt.org/vocab/unit#",
-                #         "td": "https://www.w3.org/2019/wot/td/v1"
-                #       },
-                #       "@type": "qudt:QuantityValue",
-                #       "qudt:value": data,
-                #       "qudt:unit": "unit:V"
-                # }
-                #
-                # headers = {'content-type': 'application/ld+json'}
-                #
-                # response = requests.post('http://localhost:3030/test/data?default', data=json.dumps(params),
-                #                         headers=headers, auth=(username, password))
-                print("Gelen Veriler:", data)
-                #print(response.text)
-            else:
-                print("no data")
-    except Exception as e:
-        print(f"Hata: {e}")
+    jsonld_str = requests.get(content).text
+    jsonld_content = json.loads(jsonld_str)
+    print(jsonld_content)
+
+    feature_link = jsonld_content["properties"]["Voltage"]["links"]
+    print(feature_link)
+
 
 
 if __name__ == "__main__":
